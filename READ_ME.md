36bef6da522dd7bec85da3c849b9bc09a7772c14
hmhacks <henrymanley7@gmail.com>
2021-07-07 15:58:01 -0400

    v2: requirements, readme

---
+++ READ_ME.md
@@ -0,0 +1,28 @@
+# Simulating Biased Treatment Estimates: How Networks Complicate Things
+
+## Motivations
+This repository is motivated by growing questions in field of econometrics, particularly
+surrounding how underlying networks contribute to biased treatment estimates. Consider [Miguel and Kremer 2004](https://onlinelibrary.wiley.com/doi/epdf/10.1111/j.1468-0262.2004.00481.x), which is concerned with isolating the causal effects of deworming
+programs in Kenyan schools. Such an experimental design is concerned with mitagating
+splillovers (maintaing [SUTVA](https://blogs.iq.harvard.edu/violations_of_s#:~:text=Methods%20for%20causal%20inference%2C%20in,treatments%20of%20others%20around%20him.)), but such a possibility is nearly impossible in the
+real world. As a brute-force solution to the notion of "connected" networks of pupils
+of students across treatment and control schools, a distance-based penalty is introduced
+to the OLS framework.
+
+Since, many studies, particularly in the space of developmental economics [(Banerjee, Chandrasekhar, Duflo, and Jackson)](https://economics.mit.edu/files/9070), [(Chandrasekhar and Lewis 2019)](http://stanford.edu/~arungc/CL.pdf), [(Hardy et. al)](https://arxiv.org/pdf/1904.00136.pdf) have been aimed at addressing this network dilemma in a more methodological fashion.
+
+What has yet to be developed, though, is a more general econometric framework that tackles
+this question. It seems that there is great promise in exploiting tools from probability
+and graph theory, especially in developing Monte Carlo simulations, to model the adverse effects
+of imperfect random-sampling and the subsequent violation of SUTVA. Similarly, there is room
+to explore and apply graph traversal algorithms to the challenge of recovering an unknown
+network to bolster a conservative estimate of the effect of a graph's connectedness on
+the treatment coefficient.
+
+This repository is a codebase for exploration and simulation in this general area.
+
+## Installation
+To use the code, clone the repository to a local directory. From the command line and in that
+directory, type: `pip install -r requirements.txt`. This will install repository-specific dependencies.
+From there, typing `python simulation.py` will run the main simulation functions, and yield both
+images and raw data to analyze. Further analysis is done in `analysis.py`.
--- simulation.py
+++ simulation.py
@@ -37,10 +37,8 @@ def randomGraph(n: int, s: int, c: int):
         toList = random.sample(toNodes, c)
 
         G = nx.compose(G, g)
-
         for i in range(len(toList)):
             G.add_edge(fromNodes[i], toNodes[i])
-
     return G
 
 
@@ -54,13 +52,18 @@ def sample(G, n: int):
     @param n is the number of nodes to sample from G
     """
     allNodes = list(G.nodes)
-    sampleNodes = random.sample(allNodes, n)
-
     G1 = nx.Graph()
-    G1.add_nodes_from(sampleNodes)
-    for node in sampleNodes:
-        sampleEdges = list(G.edges(node))
-        G1.add_edges_from(sampleEdges)
+
+    for i in range(2):
+        index = int(len(allNodes)/2)
+        if i == 0:
+            sampleNodes = random.sample(allNodes[index:], n)
+        else:
+            sampleNodes = random.sample(allNodes[:index], n)
+        G1.add_nodes_from(sampleNodes)
+        for node in sampleNodes:
+            sampleEdges = list(G.edges(node))
+            G1.add_edges_from(sampleEdges)
     return G1
 
 
@@ -87,26 +90,36 @@ def simulate(S: int, N: int, C: int, K: int, iterations = 10):
     of each N, S, C, and K.
     """
     assert N%10 == 0
-    print(N)
     data = pd.DataFrame(columns = ["N", "S", "C", "K", "Bias"])
     for s in range(2, S + 1):
         for c in range(1, C + 1):
-            for n in range(10, N + 10, 10):
+            for n in range(50, N + 10, 10):
                 G = randomGraph(n, s, c)
                 for k in range(1, K + 1):
                     for i in range(1, iterations + 1):
                         paramGraph = sample(G, k)
+                        print(k)
+                        drawGraph(paramGraph)
                         bias = not nx.is_connected(paramGraph)
                         row = {"N": n, "S": s, "C": c, "K": k, "Bias": bias}
                         data = data.append(row, ignore_index = True)
     return data
 
 
-if __name__ == "__main__":
-    # drawGraph(g1)
-    d = simulate(S = 2, N = 100, C = 5, K = 10)
-    print(d)
-    print(d.head(20))
+def distribution(data):
+    """
+    From simulation pandas dataframe, where each row represents a single bootstrap,
+    compute and visualize distribution of "bias". Note, bias is an instance of
+    one or more edges in the underlying network being drawn from treatment and control
+    nodes.
 
-    df = d.groupby(['N', 'S', 'C', 'K'])['Bias'].sum().reset_index()
-    print(df.head(20))
+    @param data is the aforementioned pandas dataframe- the product of simulate()
+    """
+    data = data.groupby(['N', 'S', 'C', 'K'])['Bias'].sum().reset_index()
+    return data
+
+
+if __name__ == "__main__":
+    d = simulate(S = 2, N = 50, C = 10, K = 10)
+    d = distribution(d)
+    print(d)
